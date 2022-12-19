239fbfa9d9b68828e78f4aab42b65d678425d9a5
d4nzS <dzianis.karym@gmail.com>
2022-12-19 14:57:09 +0300

    add server validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -17,6 +19,15 @@ exports.getPosts = (req, res, next) => {
 
 exports.createPost = (req, res, next) => {
   const { title, content } = req.body;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect',
+      errors: errors.array()
+    });
+  }
 
   res.status(201).json({
     message: 'Post created successfully!',
